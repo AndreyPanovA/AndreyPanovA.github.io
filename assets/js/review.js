85145754363e69816e0124717241237b886f5a69
AndreyPanovA <panov3107@mail.ru>
2020-06-20 21:50:49 +0300

    reviw_demo

--- assets/js/review.js
+++ assets/js/review.js
@@ -1,62 +1,46 @@
 // alert("cool");
+let body = document.querySelector(".scrollview");
 const url = "https://larsonv.ru/mobile.php";
 const data = { c: "Review", m: "getMore", from: "0" };
+async function getReviews() {
+  const res = await fetch("https://larsonv.ru/mobile.php", {
+    method: "POST",
+    body: JSON.stringify({ c: "Review", m: "getMore" }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  return await res.json();
+}
 
-fetch("https://larsonv.ru/mobile.php", {
-  method: "POST",
-  body: JSON.stringify({ c: "Review", m: "getMore", from: "0" }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+getReviews().then((res) => {
+  let fioArr = [];
+  let componentAll = ``;
+  res.forEach((elem, idx) => {
+    let fio = `${elem.f} ${elem.i} ${elem.o}`;
+    console.log(res);
+    // arr.push(fio);
+    // if (elem.review == "") {
+    let component = `
+    <div class="shad__reviw">
+    Оценка: ${elem.rating}
+    — ${elem.f} ${elem.i} ${elem.o}
+     
+    </div>
+    `;
+    // <div class="little">Отзыв: ${elem.review}<div/>
+    // }
+    // else {
+    // let component = `
+    //       <div class="shad__reviw">
+    //       Оценка: ${elem.rating}
+    //       — ${elem.f} ${elem.i} ${elem.o}
+    //       </div>
 
-// getResourse("https://larsonv.ru/mobile.php").then((response) => {
-//   let actMass = [];
-//   response.forEach((element, i) => {
-//     console.log(element);
-//   });
-// });
+    //       `;
+    //   }
 
-// const test = getResourse(data);
-// test.then((elem) => {
-//   console.log(elem);
-// });
-
-// class Service {
-//   _apiBase = "https://larsonv.ru/review?c=Review&m=getMore&from=0";
-//   async getResourse(url) {
-//     const res = await fetch(url);
-//     const body = await res.json();
-//     return body;
-//   }
-//   async getResourses(data) {
-//     const res = await fetch(this._apiBase, {
-//       method: "POST",
-//       body: JSON.stringify(data),
-//       headers: {
-//         "Content-type": "application/json; charset=UTF-8",
-//       },
-//     });
-//     const response = await res.json();
-//     return response;
-//   }
-//   async getAllStoks() {
-//     const res = await this.getResourses({
-//       ...Chanels.stoks,
-//       ...Chanels.connection,
-//     });
-//     return res;
-//   }
-//   getHistory = async () => {
-//     const res = await this.getResourses({
-//       ...Chanels.history,
-//       ...Chanels.connection,
-//     });
-//     return res;
-//   };
-// }
-
-// const review = new Service();
-// alert(review.getResourse(review._apiBase));
+    componentAll += component;
+    body.innerHTML = componentAll;
+  });
+});
