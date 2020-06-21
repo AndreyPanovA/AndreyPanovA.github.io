d1c6b5ff051e50d0f239cc04b130f8c8b78d976f
AndreyPanovA <panov3107@mail.ru>
2020-06-21 17:37:58 +0300

    kuzov

--- kuzov/slider/slider.js
+++ kuzov/slider/slider.js
@@ -1,16 +1,20 @@
-const slider = document.querySelector('.swiper-container');
-
-let mySwiper = new Swiper(slider, {
-	slidesPerView: 3,
-	spaceBetween: 10,
-	loop: true,
-	effect: 'fade',
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-})
+const left = document.querySelector(".btn_left");
+const right = document.querySelector(".btn_right");
+const slides = document.querySelectorAll(".slide");
+const slidesBg = document.querySelectorAll(".slide_bg");
+const slider = document.querySelector(".slider");
+const slider = document.querySelector(".swiper-container");
+// let mySwiper = new Swiper(slider, {
+//   slidesPerView: 3,
+//   spaceBetween: 10,
+//   loop: true,
+//   effect: "fade",
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
