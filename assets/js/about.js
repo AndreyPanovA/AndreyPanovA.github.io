183b0f770ecafd379111968fbfa3c1e4c5432402
AndreyPanovA <panov3107@mail.ru>
2020-06-22 21:41:33 +0300

    may be

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,82 +1,25 @@
-// let burgerBtn = document.querySelector(".burger");
-// let menu = document.querySelector(".links");
-// let size = window.innerWidth;
-// let modalClose = document.querySelector(".modalClose");
-// let line1 = document.querySelector(".line1");
-// let line2 = document.querySelector(".line2");
-// let line3 = document.querySelector(".line3");
-// let sectionContent = document.querySelector(".sectionContent");
-// let logo = document.querySelector(".logo");
-
 window.addEventListener("load", function () {
   document.querySelector("body").classList.add("loaded");
 });
 setTimeout(() => {
   document.querySelector("body").classList.add("loaded");
 }, 8000);
-
-// function mediaQuery() {
-//   size = window.innerWidth;
-//   if (size < "1370") {
-//     menu.classList.add("close");
-//   } else {
-//     menu.classList.remove("close");
-//   }
-//   burgerBtn.classList.remove("burgActive");
-//   line1.classList.remove("first");
-//   line2.classList.remove("second");
-//   line3.classList.remove("third");
-//   document.body.classList.remove("scroll");
-//   sectionContent.classList.remove("blur");
-//   logo.classList.remove("blur");
-//   modalClose.classList.remove("modalActive");
-//   modalClose.style.backgroundColor = "lime !important";
-// }
-// burgerBtn.addEventListener("click", () => {
-//   modalClose.classList.toggle("modalActive");
-//   menu.classList.toggle("close");
-//   burgerBtn.classList.toggle("burgActive");
-//   //   burgerBtn.classList.toggle("hovBtn");
-//   line1.classList.toggle("first");
-//   line2.classList.toggle("second");
-//   line3.classList.toggle("third");
-//   document.body.classList.toggle("scroll");
-//   sectionContent.classList.toggle("blur");
-//   logo.classList.toggle("blur");
-//   modalClose.addEventListener("click", () => {
-//     mediaQuery();
-//     // modalClose.classList.remove("modalActive");
-//     //
-//     // burgerBtn.classList.remove("burgActive");
-//     // line1.classList.remove("first");
-//     // line2.classList.remove("second");
-//     // line3.classList.remove("third");
-//     // document.body.classList.remove("scroll");
-//     // sectionContent.classList.remove("blur");
-//     // logo.classList.remove("blur");
-//   });
-// });
-// document.addEventListener("DOMContentLoaded", () => {
-//   mediaQuery();
-// });
-window.onresize = function (event) {
-  size = window.innerWidth;
-  mediaQuery();
-  // modalClose.classList.remove("modalActive");
-};
-
 const burg = document.querySelector(".burger");
 const menue = document.querySelector("nav");
-const closer = document.querySelector(".closer");
+const closer = document.querySelector(".modalClose");
+const body = document.querySelector(".blur_container");
 burg.addEventListener("click", () => {
   menue.classList.toggle("menuActive");
   burg.classList.toggle("move");
   burg.classList.toggle("xer");
+  body.classList.toggle("blur");
+  body.style.border = "20px solid red;";
   closer.style.display = "block";
 });
 closer.addEventListener("click", () => {
   closer.style.display = "none";
   burg.classList.remove("move");
+  burg.classList.remove("xer");
   menue.classList.remove("menuActive");
-  // burg.classList.remove("xer");
+  body.classList.remove("blur");
 });
