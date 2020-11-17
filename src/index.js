5e6146e05b534e1c9cccb22cb6c7d390c73a6e3a
AndreyPanovA <a.andrey.panov@gmail.com>
2020-11-17 19:27:58 +0300

    menui

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { Header } from "./components/header/Header";
 import { Toolbar } from "./components/toolbar/Toolbar";
 import { Formula } from "./components/formula/Formula";
 import { Table } from "./components/table/Table";
+// import "./js/libs/mo.min.js"
 
 const excel = new Excel("#app", {
     components: [Header, Toolbar, Formula, Table],
 });
 
 
-const burger = document.querySelector(".burger")
+// const burger = document.querySelector(".burger")
 const menu = document.querySelector(".navigation")
 const tabLink =document.querySelectorAll(".tab-link")
+const body =document.querySelector("body")
 
 
 tabLink.forEach((elem, idx)=> {
@@ -23,7 +25,112 @@ tabLink.forEach((elem, idx)=> {
         elem.classList.add("active-tab")
     })
 })
-burger.addEventListener("click",()=> {
-menu.classList.toggle("nav-show")
-})
+// burger.addEventListener("click",()=> {
+// menu.classList.toggle("nav-show")
+// })
 
+
+// 
+
+var timelineOpen = new mojs.Timeline({ speed: 1.5 });
+var timelineClose = new mojs.Timeline({ speed: 2 });
+
+var _strokeWidth;
+var RADIUS = 15;
+var hamburger = document.querySelector("#hamburger-open");
+
+var spans = document.getElementsByClassName("spans");
+var spanOne = document.querySelector("#spanOne");
+var spanTwo = document.querySelector("#spanTwo");
+var spanThree = document.querySelector("#spanThree");
+
+var modalMenu = document.querySelector(".modal-menu");
+
+var burst1 = new mojs.Burst({
+  parent: hamburger,
+  x: "50%",
+  y: "50%",
+  angle: { 0: 90 },
+  radius: { 30: 45 },
+  count: 3,
+  children: {
+    shape: "circle",
+    radius: RADIUS,
+    scale: { 1: 0 },
+    fill: ["#ff4338", "#00b3e3", "#3cd52e"],
+    duration: 2000,
+    easing: "quad.out"
+  }
+});
+
+var burst2 = new mojs.Burst({
+  parent: hamburger,
+  x: "50%",
+  y: "50%",
+  angle: { 0: 90 },
+  radius: { 30: 45 },
+  count: 3,
+  children: {
+    shape: "circle",
+    radius: RADIUS,
+    scale: { 0: 1 },
+    strokeWidth: { 1: 3 },
+    opacity: { 1: 0 },
+    fill: "transparent",
+    stroke: ["#ff4338", "#00b3e3", "#3cd52e"],
+    duration: 2000,
+    easing: "quad.out"
+  }
+});
+
+// OPEN
+var openBackground = new mojs.Shape({
+  fill: "#111820",
+  scale: { 0: 8.5 },
+  radius: 200,
+  delay: 500,
+  easing: "cubic.out",
+  backwardEasing: "ease.out",
+  duration: 2000
+});
+
+burst1.el.style.zIndex = 90000;
+openBackground.el.style.zIndex = 100;
+
+// check if the hamburger's been crossed
+let cross = spanOne.classList.contains("white");
+
+//timeline with burst and background open
+timelineOpen.add(burst1, burst2, openBackground);
+//timeline with background close
+timelineClose.add(openBackground);
+
+//click on the hamburger
+hamburger.addEventListener("click", function(e) {
+  // check if the menu is a cross
+  var cross = spanOne.classList.contains("white");
+
+  modalMenu.classList.toggle("show");
+  body.classList.toggle("stop")
+
+
+  if (cross) {
+    timelineClose.playBackward();
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].classList.remove("white");
+    }
+    spanOne.classList.remove("spanOneRotate");
+    spanTwo.classList.remove("spanTwoRotate");
+    spanThree.classList.remove("spanThreeHide");
+  } else {
+    timelineOpen.play();
+
+    for (var i = 0; i < spans.length; i++) {
+      spans[i].classList.add("white");
+    }
+
+    spanOne.classList.add("spanOneRotate");
+    spanTwo.classList.add("spanTwoRotate");
+    spanThree.classList.add("spanThreeHide");
+  }
+});
