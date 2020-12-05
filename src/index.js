f66a74b1221527ef19e233a5e1c2943af6893c0e
AndreyPanovA <a.andrey.panov@gmail.com>
2020-12-05 21:12:52 +0300

    t

--- src/index.js
+++ src/index.js
@@ -6,57 +6,72 @@ window.addEventListener('scroll',Scroll);
 import {dropdown,dropdownItem} from "./js/dropdown"
 import Burger from "./js/hamburger"
 
+(function Circle(e) {
+  var burst1 = new mojs.Burst({
+      parent: hamburger,
+      x: "50%",
+      y: "50%",
+      angle: { 0: 90 },
+      radius: { 30: 45 },
+      count: 3,
+      children: {
+        shape: "circle",
+        radius: RADIUS,
+        scale: { 1: 0 },
+        fill: ["#ff4338", "#00b3e3", "#3cd52e"],
+        duration: 2000,
+        easing: "quad.out"
+      }
+    });
+    var burst2 = new mojs.Burst({
+      parent: hamburger,
+      x: "50%",
+      y: "50%",
+      angle: { 0: 90 },
+      radius: { 30: 45 },
+      count: 3,
+      children: {
+        shape: "circle",
+        radius: RADIUS,
+        scale: { 0: 1 },
+        strokeWidth: { 1: 3 },
+        opacity: { 1: 0 },
+        fill: "transparent",
+        stroke: ["#ff4338", "#00b3e3", "#3cd52e"],
+        duration: 2000,
+        easing: "quad.out"
+      }
+    });
+    // OPEN
+    var openBackground = new mojs.Shape({
+      fill: "#111820",
+      scale: { 0: 8.5 },
+      radius: 200,
+      delay: 500,
+      easing: "cubic.out",
+      backwardEasing: "ease.out",
+      duration: 2000
+    });
+    
+    burst1.el.style.zIndex = 90000;
+    openBackground.el.style.zIndex = 100;
+    openBackground.el.style.position = "fixed";
+    let cross = spanOne.classList.contains("white");
+    timelineOpen.add(burst1, burst2, openBackground);
+    timelineClose.add(openBackground);
+    hamburger.addEventListener("click", Burger);
+})()
 
-var burst1 = new mojs.Burst({
-  parent: hamburger,
-  x: "50%",
-  y: "50%",
-  angle: { 0: 90 },
-  radius: { 30: 45 },
-  count: 3,
-  children: {
-    shape: "circle",
-    radius: RADIUS,
-    scale: { 1: 0 },
-    fill: ["#ff4338", "#00b3e3", "#3cd52e"],
-    duration: 2000,
-    easing: "quad.out"
-  }
-});
-var burst2 = new mojs.Burst({
-  parent: hamburger,
-  x: "50%",
-  y: "50%",
-  angle: { 0: 90 },
-  radius: { 30: 45 },
-  count: 3,
-  children: {
-    shape: "circle",
-    radius: RADIUS,
-    scale: { 0: 1 },
-    strokeWidth: { 1: 3 },
-    opacity: { 1: 0 },
-    fill: "transparent",
-    stroke: ["#ff4338", "#00b3e3", "#3cd52e"],
-    duration: 2000,
-    easing: "quad.out"
-  }
-});
-// OPEN
-var openBackground = new mojs.Shape({
-  fill: "#111820",
-  scale: { 0: 8.5 },
-  radius: 200,
-  delay: 500,
-  easing: "cubic.out",
-  backwardEasing: "ease.out",
-  duration: 2000
-});
-
-burst1.el.style.zIndex = 90000;
-openBackground.el.style.zIndex = 100;
-openBackground.el.style.position = "fixed";
-let cross = spanOne.classList.contains("white");
-timelineOpen.add(burst1, burst2, openBackground);
-timelineClose.add(openBackground);
-hamburger.addEventListener("click", Burger);
+// 
+const next = document.querySelector(".next")
+const chengeModule = document.querySelectorAll(".chenge-module")
+let i = 0
+next.addEventListener("click", (e)=> {
+  chengeModule.forEach((el,idx)=> {
+    i++
+    el.classList.add("off")
+    if (el.id==idx) {
+      el.classList.remove("off")
+    }
+  })
+})
