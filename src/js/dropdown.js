5ce87c17177d0de7e080b52d572e57566c0ab07d
AndreyPanovA <a.andrey.panov@gmail.com>
2020-12-05 22:19:20 +0300

    prev

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,8 +1,16 @@
 import {dropdown,dropdownItem} from "../module"
-dropdown.forEach((el,idx)=> {
-    el.addEventListener("click", ()=> {
-        dropdownItem[idx].classList.toggle("offItem")
-      })
+
+const servMenu = document.getElementsByClassName('frame__tagline')[2];
+const drop = document.getElementsByClassName('dropdown')[0];
+
+servMenu.addEventListener('mouseover',(e) => {
+  drop.classList.add('on');
+})
+drop.addEventListener('mouseover',(e) => {
+  drop.classList.add('on');
+})
+drop.addEventListener('mouseout',(e) => {
+  drop.classList.remove('on');
 })
 
 export {
